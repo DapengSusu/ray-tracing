25e360a64e6e92ea91c8e26589d445af05c6eb9f
Jumper <dapengsusu@qq.com>
2025-10-22 12:35:17 +0800

    使用 rayon crate 做并行处理，并改为缓冲写入
    生成 images/image2.ppm 用时约 45ms
    添加可视化执行过程

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,6 +1,12 @@
-use std::time::Instant;
+use std::{
+    io::{self, BufWriter, Write},
+    sync::atomic::{AtomicUsize, Ordering},
+    time::Instant,
+};
 
-fn main() {
+use rayon::prelude::*;
+
+fn main() -> Result<(), io::Error> {
     // Image
     let image_width: usize = 256;
     let image_height: usize = 256;
@@ -8,24 +14,55 @@ fn main() {
     // Start timer
     let now = Instant::now();
 
+    // Writer
+    let stdout = io::stdout();
+    let mut writer = BufWriter::new(stdout.lock());
+
+    // Remaining lines
+    let remaining_lines = AtomicUsize::new(image_height);
+
     // Render
-    println!("P3\n{} {}\n255\n", image_width, image_height);
+    writer.write_all(b"P3\n")?;
+    writer.write_all(format!("{image_width} {image_height}\n").as_bytes())?;
+    writer.write_all(b"255\n")?;
 
-    for j in 0..image_height {
-        for i in 0..image_width {
-            let r = i as f64 / (image_width - 1) as f64;
-            let g = j as f64 / (image_height - 1) as f64;
-            let b = 0.;
+    let rows = (0..image_height)
+        .into_par_iter()
+        .map(|j| {
+            let row = (0..image_width)
+                .map(|i| {
+                    let r = i as f64 / (image_width - 1) as f64;
+                    let g = j as f64 / (image_height - 1) as f64;
+                    let b = 0.;
 
-            let r = (255.999 * r) as u8;
-            let g = (255.999 * g) as u8;
-            let b = (255.999 * b) as u8;
+                    (
+                        (255.999 * r) as u8,
+                        (255.999 * g) as u8,
+                        (255.999 * b) as u8,
+                    )
+                })
+                .collect::<Vec<_>>();
 
-            println!("{} {} {}\n", r, g, b);
-        }
+            let remaining = remaining_lines.fetch_sub(1, Ordering::Relaxed);
+            eprint!("\r\x1B[KScanlines remaining: {}", remaining - 1);
+
+            let mut row_bytes = Vec::with_capacity(row.len() * 10);
+            for (r, g, b) in &row {
+                row_bytes.extend_from_slice(format!("{r} {g} {b}\n").as_bytes());
+            }
+
+            row_bytes
+        })
+        .collect::<Vec<_>>();
+
+    for row in &rows {
+        writer.write_all(row)?;
     }
 
     // End timer
+    eprint!("\r\x1B[K");
     let elapsed = now.elapsed();
-    eprintln!("Elapsed time: {}ms", elapsed.as_millis());
+    eprintln!("\nDone. Elapsed time: {}ms", elapsed.as_millis());
+
+    Ok(())
 }
